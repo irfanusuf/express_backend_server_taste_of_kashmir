b94d397b3064a430d87f418768b3b49b8c37bb38
Irfan usuf <irfanusuf33@gmail.com>
2024-03-30 15:12:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ require('dotenv').config();
 
 const app = express();                       //intiating express app 
 const port = 4000               //defining port 
-const url = process.env.MONGO_URL_DEV;       // declaring url
+const url =  "mongodb+srv://irfanusuf33:ester327811@cluster0.g8zmr53.mongodb.net/recipe-server"    // declaring url
 
 app.use(cors());                             // using cors middleware which enables cross origin resource sharing 
 app.use(bodyParser.json());                  // using bodyparser which converts body data into json 
