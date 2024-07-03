ec1cfc715286f5c6b660f60bc5ea5668ef86d5da
Irfan usuf <irfanusuf33@gmail.com>
2024-07-03 15:07:40 +0530

    Update postRecipeData.js

--- controllers/postRecipeData.js
+++ controllers/postRecipeData.js
@@ -5,9 +5,9 @@ const User = require('../models/UserModel')
 
 
 cloudinary.config({
-    cloud_name: 'dbo0xmbd7',
-    api_key: '717735839128615',
-    api_secret: 'fqcjtd3HxpH_t1dAEtqr595ULW0'
+    cloud_name:   process.env.CLOUD_NAME ,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
 });
 
 const PostRecipeData = async (req, res) => {
